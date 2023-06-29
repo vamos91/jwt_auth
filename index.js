46ffe413454f1a8583cc72bef1d0d7572d316533
simplon-marseille <acardnicolas@hotmail.com>
2023-06-29 13:27:10 +0200

    deploy v1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const port = process.env.PORT || 3001
+const path = require('path')
 require('dotenv').config()
 app.use(express.json())
 var cookieParser = require('cookie-parser')
@@ -8,10 +9,11 @@ app.use(cookieParser())
 const userRouter = require('./router/user')
 app.use('/users', userRouter)
 
-
+app.use(express.static(path.join(__dirname, 'client/build')))
+const pathToFile = path.join(__dirname, 'client/build')
 
 app.get('/', (req, res) => {
-    res.send('Hello from server')
+    res.sendFile(pathToFile, 'index.html')
 })
 
 app.listen(port, () => {
